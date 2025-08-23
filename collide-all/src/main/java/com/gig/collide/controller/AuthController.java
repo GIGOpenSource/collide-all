a578a6272c3473a310f2658037a6805fd901ae1b
whyHT <w2462298412@outlook.com>
2025-08-23 17:47:15 +0800

    缩进

--- collide-all/src/main/java/com/gig/collide/controller/AuthController.java
+++ collide-all/src/main/java/com/gig/collide/controller/AuthController.java
@@ -135,7 +135,7 @@ public class AuthController {
             
             Long userId = StpUtil.getLoginIdAsLong();
             User user = userService.getUserById(userId);
-            
+
             if (user == null) {
                 return Result.error("用户不存在");
             }
